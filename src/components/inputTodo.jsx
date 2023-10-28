9f81989ebe9a0f3dcceb515d4dc2d181a12a0bc0
vikiade00 <vikiade00@gmail.com>
2023-10-28 13:05:26 +0700

    Add UI Todo App

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -6,14 +6,13 @@ function InputTodo() {
       <form action="" className="flex gap-4 items-center">
         <div className="flex flex-col gap-2">
           <input
-            className="border pl-2 rounded-md border-gray-400 "
+            className="ring-0 pl-4 rounded-2xl shadow-sm border-gray-400 py-1"
             type="text"
-            placeholder="Task"
+            placeholder="Add some taks..."
           />
-          <input className="border" type="text" placeholder="Description" />
         </div>
         <div>
-          <button className="text-blue-500 bg-blue-50 px-3 py-1 rounded-md">
+          <button className="text-blue-500 bg-blue-50 px-3 py-1 rounded-2xl">
             + New Task
           </button>
         </div>
--- src/pages/todoPage.jsx
+++ src/pages/todoPage.jsx
@@ -4,9 +4,15 @@ import TodoList from "../components/todoList";
 
 function TodoPage() {
   return (
-    <div className="flex flex-col items-center mt-20">
-      <div className="shadow p-10 rounded-2xl">
-        <h1 className="text-center mb-5">Today's Task</h1>
+    <div className="flex flex-col justify-center items-center text-gray-600">
+      <div className="text-white text-center mb-10 mt-10">
+        <h1 className="text-3xl font-bold">
+          Every Task Brings You Closer To Success !
+        </h1>
+        <p>Small steps lead to big accomplishments.</p>
+      </div>
+      <div className="shadow-xl p-10 rounded-2xl bg-gray-50">
+        <h1 className="text-center mb-5 font-bold">Today's Task</h1>
         <InputTodo />
         <TodoList />
       </div>
